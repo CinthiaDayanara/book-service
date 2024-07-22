4ce682ff0b069055a78ac005c807bee39a0692fb
SaraSaragozin <saralizeth0711@gmail.com>
2024-07-22 12:31:03 -0500

    add mongo atlas

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const mongoose = require('mongoose');
 const bookRoutes = require('./routes/bookRoutes');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = 3012;
 
 
-mongoose.connect('mongodb://localhost:27017/books_db', {
+const uri = 'mongodb+srv://admin:admin@cluster0.acc1is2.mongodb.net/books_db?retryWrites=true&w=majority&appName=Cluster0';;
 
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
-.then(() => console.log('Conectado a MongoDB'))
-.catch((error) => console.error('Error al conectar a MongoDB:', error));
+.then(() => console.log('Conectado a MongoDB Atlas'))
+.catch((error) => console.error('Error al conectar a MongoDB Atlas:', error.message));
 
 app.use(cors({
   origin: '*', // Permite solicitudes desde esta URL
